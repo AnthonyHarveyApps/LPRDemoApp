011fcf8fc9b0eff3959c59913344f4be84ac5384
AH <AnthonyBryanHarvey@gmail.com>
2023-08-08 16:06:31 -0400

    Initial Commit

---
+++ LPRDemoApp/LPRDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LPRDemoAppApp.swift
+//  LPRDemoApp
+//
+//  Created by Anthony Harvey on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LPRDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
